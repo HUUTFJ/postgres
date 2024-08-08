MasaoFujii/postgresql

Extend postgres_fdw_get_connections to return user name

contrib/postgres_fdw/postgres_fdw--1.1--1.2.sql
@@ -11,7 +11,8 @@ DROP FUNCTION postgres_fdw_get_connections ();
 
 CREATE FUNCTION postgres_fdw_get_connections
     (IN require_verify boolean DEFAULT false, OUT server_name text,
-     OUT valid boolean, OUT used_in_xact boolean, OUT closed boolean)
+     OUT valid boolean, OUT user_name text, OUT used_in_xact boolean,
+     OUT closed boolean)
 RETURNS SETOF record
 AS 'MODULE_PATHNAME', 'postgres_fdw_get_connections_1_2'
 LANGUAGE C STRICT PARALLEL RESTRICTED;

HUUTFJ/postgres

contrib/postgres_fdw/postgres_fdw--1.1--1.2.sql
@@ -11,7 +11,8 @@ DROP FUNCTION postgres_fdw_get_connections ();
 
 CREATE FUNCTION postgres_fdw_get_connections (
     IN check_conn boolean DEFAULT false, OUT server_name text,
-    OUT valid boolean, OUT used_in_xact boolean, OUT closed boolean)
+    OUT user_name text, OUT valid boolean, OUT used_in_xact boolean,
+    OUT closed boolean)
 RETURNS SETOF record
 AS 'MODULE_PATHNAME', 'postgres_fdw_get_connections_1_2'
 LANGUAGE C STRICT PARALLEL RESTRICTED;