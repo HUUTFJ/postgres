7ccc733b2c41d876cd45e39931565e4e008961a2
Hayato Kuroda <hayato@example.com>
2025-10-11 22:12:30 +0900

    Make 009_mateviews.pl shorter

--- src/test/subscription/t/009_matviews.pl
+++ src/test/subscription/t/009_matviews.pl
@@ -19,18 +19,19 @@ $node_subscriber->start;
 my $publisher_connstr = $node_publisher->connstr . ' dbname=postgres';
 
 $node_publisher->safe_psql('postgres',
+	q{CREATE TABLE test1 (a int PRIMARY KEY, b text)});
+$node_publisher->safe_psql('postgres',
+	q{CREATE MATERIALIZED VIEW testmv1 AS SELECT * FROM test1;});
+$node_subscriber->safe_psql('postgres',
+	q{CREATE TABLE test1 (a int PRIMARY KEY, b text);});
+$node_subscriber->safe_psql('postgres',
+	q{CREATE MATERIALIZED VIEW testmv1 AS SELECT * FROM test1;});
+
+$node_publisher->safe_psql('postgres',
 	"CREATE PUBLICATION mypub FOR ALL TABLES;");
 $node_subscriber->safe_psql('postgres',
 	"CREATE SUBSCRIPTION mysub CONNECTION '$publisher_connstr' PUBLICATION mypub;"
 );
-
-$node_publisher->safe_psql('postgres',
-	q{CREATE TABLE test1 (a int PRIMARY KEY, b text)});
-$node_publisher->safe_psql('postgres',
-	q{INSERT INTO test1 (a, b) VALUES (1, 'one'), (2, 'two');});
-
-$node_subscriber->safe_psql('postgres',
-	q{CREATE TABLE test1 (a int PRIMARY KEY, b text);});
 
 $node_publisher->wait_for_catchup('mysub');
 
@@ -38,15 +39,26 @@ $node_publisher->wait_for_catchup('mysub');
 # logical decoding does produce change information for them, so we
 # need to make sure they are properly ignored. (bug #15044)
 
-# create a MV with some data
 $node_publisher->safe_psql('postgres',
-	q{CREATE MATERIALIZED VIEW testmv1 AS SELECT * FROM test1;});
+	q{INSERT INTO test1 (a, b) VALUES (1, 'one'), (2, 'two');});
+$node_publisher->safe_psql('postgres',
+	q{REFRESH MATERIALIZED VIEW testmv1;});
 $node_publisher->wait_for_catchup('mysub');
 
-# There is no equivalent relation on the subscriber, but MV data is
-# not replicated, so this does not hang.
+my $result = $node_publisher->safe_psql('postgres',
+	"SELECT count(*) FROM testmv1");
+is ($result, '2', 'materialized view on publisher has 2 rows');
 
-pass "materialized view data not replicated";
+$result = $node_subscriber->safe_psql('postgres',
+	"SELECT count(*) FROM testmv1");
+is ($result, '0', 'materialized view on subscriber d otno have ');
+
+$node_subscriber->safe_psql('postgres',
+	q{REFRESH MATERIALIZED VIEW testmv1;});
+
+$result = $node_subscriber->safe_psql('postgres',
+	"SELECT count(*) FROM testmv1");
+is ($result, '2', 'materialized view on subscriber has 2 rows after the refresh');
 
 $node_subscriber->stop;
 $node_publisher->stop;
