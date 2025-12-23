2aa2a27e3b8961c7aa4f1ca04da360e82c6cfe1b
Hayato Kuroda <kuroda.hayato@fujitsu.com>
2025-12-23 17:58:15 +0900

    Wait applying transaction if one of user-defined triggers is immutable
    Since many parallel workers apply transactions, triggers for relations can also
    be fired in parallel, which may obtain unexpected results. To make it safe,
    parallel apply workers wait for the previously dispatched transaction before
    applying changes to the relation that has mutable triggers.

--- src/backend/replication/logical/worker.c
+++ src/backend/replication/logical/worker.c
@@ -1063,6 +1063,59 @@ check_dependency_on_rel(LogicalRepRelId relid, TransactionId new_depended_xid,
 }
 
 /*
+ * Check the parallelizability of applying changes for the relation.
+ * Append the lastly dispatched transaction in in 'depends_on_xids' if the
+ * relation is parallel unsafe.
+ */
+static void
+check_dependency_for_parallel_safety(LogicalRepRelId relid,
+									 TransactionId new_depended_xid,
+									 List **depends_on_xids)
+{
+	LogicalRepRelMapEntry *relentry;
+
+	/* Quick exit if no transactions have been dispatched */
+	if (!TransactionIdIsValid(last_remote_xid))
+		return;
+
+	relentry = logicalrep_get_relentry(relid);
+
+	/*
+	 * Gather information for local triggres if not yet. We require to be in a
+	 * transaction state because system catalogs are read.
+	 */
+	if (relentry->parallel_safe == LOGICALREP_PARALLEL_UNKNOWN)
+	{
+		bool	needs_start = !IsTransactionOrTransactionBlock();
+
+		if (needs_start)
+			StartTransactionCommand();
+
+		logicalrep_rel_load(NULL, relid, AccessShareLock);
+
+		/*
+		 * Close the transaction if we start here. We must not abort because it
+		 * would release all session-level locks, such as the stream lock, and
+		 * break the deadlock detection mechanism between LA and PA. The
+		 * outcome is the same regardless of the end status, since the
+		 * transaction did not modify any tuples.
+		 */
+		if (needs_start)
+			CommitTransactionCommand();
+
+		Assert(relentry->parallel_safe != LOGICALREP_PARALLEL_UNKNOWN);
+	}
+
+	/* Do nothing for parallel safe relations */
+	if (relentry->parallel_safe == LOGICALREP_PARALLEL_SAFE)
+		return;
+
+	*depends_on_xids = check_and_append_xid_dependency(*depends_on_xids,
+													   &last_remote_xid,
+													   new_depended_xid);
+}
+
+/*
  * Check dependencies related to the current change by determining if the
  * modification impacts the same row or table as another ongoing transaction. If
  * needed, instruct parallel apply workers to wait for these preceding
@@ -1120,6 +1173,8 @@ handle_dependency_on_change(LogicalRepMsgType action, StringInfo s,
 			check_dependency_on_replica_identity(relid, &newtup,
 												 new_depended_xid,
 												 &depends_on_xids);
+			check_dependency_for_parallel_safety(relid, new_depended_xid,
+												 &depends_on_xids);
 			break;
 
 		case LOGICAL_REP_MSG_UPDATE:
@@ -1127,12 +1182,18 @@ handle_dependency_on_change(LogicalRepMsgType action, StringInfo s,
 										   &newtup);
 
 			if (has_oldtup)
+			{
 				check_dependency_on_replica_identity(relid, &oldtup,
 													 new_depended_xid,
 													 &depends_on_xids);
+				check_dependency_for_parallel_safety(relid, new_depended_xid,
+													 &depends_on_xids);
+			}
 
 			check_dependency_on_replica_identity(relid, &newtup,
 												 new_depended_xid,
+												 &depends_on_xids);
+			check_dependency_for_parallel_safety(relid, new_depended_xid,
 												 &depends_on_xids);
 			break;
 
@@ -1140,6 +1201,8 @@ handle_dependency_on_change(LogicalRepMsgType action, StringInfo s,
 			relid = logicalrep_read_delete(&change, &oldtup);
 			check_dependency_on_replica_identity(relid, &oldtup,
 												 new_depended_xid,
+												 &depends_on_xids);
+			check_dependency_for_parallel_safety(relid, new_depended_xid,
 												 &depends_on_xids);
 			break;
 
@@ -1153,8 +1216,13 @@ handle_dependency_on_change(LogicalRepMsgType action, StringInfo s,
 			 * modified the same table.
 			 */
 			foreach_int(truncated_relid, remote_relids)
+			{
 				check_dependency_on_rel(truncated_relid, new_depended_xid,
 										&depends_on_xids);
+				check_dependency_for_parallel_safety(truncated_relid,
+													 new_depended_xid,
+													 &depends_on_xids);
+			}
 
 			break;
 
